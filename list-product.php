427e3191878e2841f8b27b9913009726f6052192
khongying <kongying_99_kunket@hotmail.com>
2017-02-20 13:33:31 +0700

    update count

--- list-product.php
+++ list-product.php
@@ -1,3 +1,25 @@
+<script>
+  $( document ).ready(function() {
+      $(".number-product").blur(function() {
+        let _name = $(this).attr("name");
+        let _id = _name.replace("product-", "");
+        let _value = $(this).val();
+        let in_value = $("#form-cart input[name='product-"+_id+"']").attr("value");
+        let c_cart = $("#count_cart").attr("count");
+        let p_cart = c_cart - (in_value - _value);
+        $("#count_cart").attr("count", p_cart);
+        $("#count_cart").html(p_cart);
+        $("#form-cart input[name='product-"+_id+"']").attr("value",_value);
+        console.log(_value +' > '+in_value);
+
+      });
+  });
+
+
+</script>
+
+
+
 <?php
 
 require_once('admin/conDB/conDB.php');
@@ -24,7 +46,7 @@ require_once('function-products.php');
               print '<td><img class="img-rounded img-mini" src="admin/product_image/'.$_info['image'].'"/></td>';
               print '<td>'.$_info['name'].'</td>';
               print '<td>'.$_info['price'].'</td>';
-              print '<td><input type="nunber" min="0" max="99" value="'.$value.'" size="1px"</td>';
+              print '<td><input name="product-'.$_id.'" class="number-product" type="number" min="0" max="99" value="'.$value.'"</td>';
               print '</tr>';
 
             }
